122653e76fb1a81f6ef1b5cafcfb5ada6f0f43c7
LavleenAgrawal <70798256+LavleenAgrawal@users.noreply.github.com>
2021-06-07 03:08:54 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1> JavaScript and React.js </h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha. We covered
+          everything from scratch including JavaScript, React.js and HTML.
+        </p>
+      </div>
+
+      <div className="note">
+        <h1> WebDev with Bootstrap </h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha. We covered
+          everything from scratch including HTML, CSS and Bootstrap.
+        </p>
+      </div>
+
+      <div className="note">
+        <h1> Python and ML </h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha. We covered
+          everything from scratch including Python and ML concepts.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
